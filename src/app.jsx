8a1156c94982fd5f5a76266f27a15d631c7e72c3
Lyddem <lydiademi@gmail.com>
2018-01-08 08:43:06 -0800

    First commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var createReactClass = require('create-react-class');
 
-var Hello = React.createClass({
+var Hello = createReactClass({
   render: function() {
-    return <h1 className="red">
+    return (<h1 className="red">
       Hello!
     </h1>
+    )
   }
 });
 
-var element = React.createElement(Hello, {});
-ReactDOM.render(element, document.querySelector('.container'));
+// var element = createElement(Hello, {});
+ReactDOM.render( <Hello />, document.querySelector('.container'));
